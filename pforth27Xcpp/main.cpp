96b22866ec5e8b049285fdb691e7c8cf15cc8705
Brian Beckman <bc.beckman@gmail.com>
2013-05-04 17:31:07 -0700

    Initial Commit

---
+++ pforth27Xcpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pforth27Xcpp
+//
+//  Created by Reb Cabin on 5/4/13.
+//  Copyright (c) 2013 Reb Cabin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
